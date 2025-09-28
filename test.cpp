0163d059f3f10ab04df82bfc5112b7091b388d3e
Anurag-ABH <dasanurag0606@gmail.com>
2025-09-28 21:44:33 +0000

    Modifing Files

--- test.cpp
+++ test.cpp
@@ -3,7 +3,30 @@
 #include<ctime>
 using namespace std;
 
-int main () {
+    int main() {
     srand(time(NULL));
-    cout << rand() % 5 << endl<< rand() % 5 << endl << rand() % 5 << endl << rand() % 5;
+    int n, locked, score = 0, live = 3;
+    while (n != 'x' || n != 'X')
+    {
+        cout << "\nScore: " << score << "\t\tLives: " << live << endl;
+        cout << "\nGuess The Number Between 1-10 :  ";
+        cin >> n;
+        locked = rand() % 10 + 1;
+        if(n == locked) {
+            cout << "\nYou are correct";
+            score += 5;
+            if (live < 3) {
+                live++;
+            }
+        } else if(n != locked) {
+            cout << "\nYou are wrong! The number was: " << locked;
+            if (live > 0) {
+                live--;
+            } else {
+                cout << "\nYou have no lives! Good Bye!";
+                break;
+            }
+        }
+    };
+        
 }
